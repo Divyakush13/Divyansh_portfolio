6ba0a48fa54e40829b1c1bdf3efd274620ab932e
FIRST_NAME LAST_NAME <kushwahdivyansh2@gmail.com>
2023-08-13 12:37:11 +0530

    jkn

--- src/componentes/Contect.js
+++ src/componentes/Contect.js
@@ -1,28 +1,93 @@
-import React from 'react';
-import { useState } from 'react';
-import "./Contect.css"
-import TextField from '@mui/material/TextField';
-
+import React, { useState } from 'react';
+import "./Contect.css";
 const Contect = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://formspree.io/f/mvojkbyd', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setShowSuccessMessage(true);
+        // Clear form fields after successful submission
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      } else {
+        console.error('Error sending email');
+        alert('Error sending email. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('Error sending email. Please try again later.');
+    }
+  };
 
   return (
-
     <>
       <div className='main'>
         <div className='form'>
-          <h1 className='contectHead'>contect me</h1>
-          {/* <TextField className='textfield' id="outlined-basic" label="Outlined" variant="outlined" />  
-                <TextField className='textfield' id="outlined-basic" label="Outlined" variant="outlined" />  
-                <TextField className='textfield' id="outlined-basic" label="Outlined" variant="outlined" />   */}
-          <input className='textfield' type='text' placeholder='Full Name' />
-          <input className='textfield' type='email' placeholder='Email' />
-          <textarea className='massege' type='text' placeholder='Write massage here...' />
-         <button className='sentbtn' >Send Massage</button>
+          <h1 className='contectHead'>Contact Me</h1>
+          <input
+            className='textfield'
+            type='text'
+            placeholder='Full Name'
+            name='name'
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            className='textfield'
+            type='email'
+            placeholder='Email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <textarea
+            className='massege'
+            placeholder='Write message here...'
+            name='message'
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <button className='sentbtn' onClick={handleSubmit}>Send Message</button>
         </div>
 
-        <h className='slogan' >Let's make something new diffrent and more meaningfull or make thingh more visual and conceptual? <span>Just say Hello !</span></h>
+        {showSuccessMessage && (
+          <div className="success-message">
+            <p>Email sent successfully!</p>
+          </div>
+        )}
 
-
+        <h1 className='slogan'>
+          Let's make something new, different, and more meaningful. <span>Just say Hello!</span>
+        </h1>
       </div>
     </>
   );
