0b4dd3ae48daba9861e7c00854b9d96ddad28ad5
FIRST_NAME LAST_NAME <kushwahdivyansh2@gmail.com>
2023-08-19 19:50:52 +0530

    resume

--- src/componentes/Description.js
+++ src/componentes/Description.js
@@ -4,7 +4,12 @@ import girl from "../Images/girl.png"
 import shap from "../Images/s.png"
 import Aos from "aos";
 import "aos/dist/aos.css";
+
 const Description = () => {
+  const change = () => {
+    const downloadLink = document.querySelector('.download');
+    downloadLink.style.backgroundColor = '';
+};
   useEffect(() => {
     Aos.init({ duration: 2000 });
     }, []);
@@ -14,7 +19,7 @@ const Description = () => {
              <h1 data-aos="fade-in"  data-aos-anchor-placement="center-bottom">I'm Divyansh <span data-aos="fade-in"  data-aos-anchor-placement="center-bottom">Kushwaha</span></h1>
              <p  data-aos="fade-in"  data-aos-anchor-placement="center-bottom" >This is my personal portfolio 
              <br/>Detail about me and my experiences</p>
-             <a href='Resume.pdf' download='Resume.pdf'>DOWNLOAD CV</a>
+             <a className="download" href='Resume.pdf' download='Resume.pdf' onClick={change}>DOWNLOAD CV</a>
           </div>
           <div className='image'>
                 <img className='shap' src={shap}/>
